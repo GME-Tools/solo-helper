6221955ad115ecd1466f4f9c2fbb59f2c2a94591
Benjamin Coudrin <benjamin.coudrin@gmail.com>
2022-07-19 08:05:40 +0200

    Add firebase link to HelpersList

--- src/components/HelpersList/HelpersList.js
+++ src/components/HelpersList/HelpersList.js
@@ -1,7 +1,24 @@
 import { useFirebase } from "context/FirebaseContext";
+import { useEffect, useState } from "react";
 
 export default function HelpersList() {
   const firebase = useFirebase();
+  const [helpers, setHelpers] = useState(null);
+
+  useEffect(() => {
+    firebase.getCurrentUser().then(doc => {
+      if (doc.data().hasOwnProperty('helpers')) {
+        setHelpers(doc.data().helpers);
+      }
+      else {
+        setHelpers([]);
+      }
+    })
+    .catch(err => {
+      firebase.setCurrentUserData([])
+    });
+  }, [firebase]);
+
 
   return (
     <div className="container">
