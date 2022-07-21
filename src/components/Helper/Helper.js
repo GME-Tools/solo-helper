06c7b96d2e3dfb7828758ddd07f3e40ba3f45989
Mathieu Torres <45943028+Myrdinn31@users.noreply.github.com>
2022-07-21 09:02:02 +0000

    Adding ComboBox, Button and TextArea

--- src/components/Helper/Helper.js
+++ src/components/Helper/Helper.js
@@ -1,13 +1,23 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Container, Select, FormControl, InputLabel, MenuItem } from "@mui/material";
+import { Container, Autocomplete, TextField, Button } from "@mui/material";
 import { useFirebase } from "context/FirebaseContext";
+
+/* function onClickFunctions(event) {
+    console.log("test");
+} */
 
 export default function Helper() {
   const firebase = useFirebase();
   const { id, token } = useParams();
   const [isAuth, setIsAuth] = useState(false);
   const [data, setData] = useState({})
+  const [tfValue, setTFValue] = useState("");
+  const [functionSelected, setFunctionSelected] = useState("");
+  const functions = [
+  { label: 'Fate', id: 1 },
+  { label: 'Event', id: 2 },
+];
 
   useEffect(() => {
     firebase.getDocument("helpers",id).then(doc => setData(doc.data()));
@@ -26,23 +36,46 @@ export default function Helper() {
   
   return (
     <Container>
-      <FormControl fullWidth>
-        <InputLabel id="functions-label">Functions</InputLabel>
-        <Select
-    labelId="functions-label"
-    id="functions"
-    // value={function}
-    label="Functions"
-    // onChange={handleChange}
-  >
-          <MenuItem
-            // value={Fate} 
-            >Fate</MenuItem>
-          <MenuItem
-            // value={Event}
-            >Event</MenuItem>
-        </Select>
-      </FormControl>
+      <Autocomplete
+        autoComplete
+        autoSelect
+        disablePortal
+        id="combo-box-functions"
+        options={functions}
+        onInputChange={(event, inputValue) => // setTFValue(inputValue);
+        setFunctionSelected(inputValue)}
+        sx={{ width: 300 }}
+        renderInput={(params) => 
+      <TextField {...params} label="Functions" />}
+/>
+
+      <Button
+        variant="contained"
+        onClick={()=>
+          { let random = Math.floor(Math.random() * 20 + 1);
+            if(functionSelected === "Fate") {
+            if (random > 10) {
+              setTFValue("Oui (" + random + ")")
+            } else {
+              setTFValue("Non (" + random + ")")
+            }
+          } else {
+            setTFValue("Event pas encore intégré")
+          }}}
+        //onClick={onClickFunctions}
+        >Dice
+      </Button>
+
+      <TextField
+        multiline
+        margin="normal"
+        id="outlined-functions"
+        variant="outlined"
+        value={tfValue}
+        onChange={(newValue) => setTFValue(newValue.target.value)}
+        InputProps={{
+          readOnly: true,
+        }} />
     </Container>
   )
 }
